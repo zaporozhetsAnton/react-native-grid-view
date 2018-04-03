8706984871b44e1bf518f2973d7190e72fcd3364
anton <anton.zaporozhets@gmail.com>
2018-04-03 14:51:46 +0300

    add ES6 syntax

--- index.js
+++ index.js
@@ -1,63 +1,88 @@
 'use strict';
 
-import React from 'react';
-
+import React, { Component } from 'react'
 import {
   AppRegistry,
   View,
   StyleSheet,
-  ListView,
+  FlatList,
 } from 'react-native';
 
-var CollectionView = React.createClass({
-    groupItems: function(items, itemsPerRow) {
-        var itemsGroups = [];
-        var group = [];
-        items.forEach(function(item) {
-          if (group.length === itemsPerRow) {
-            itemsGroups.push(group);
-            group = [item];
-          } else {
-            group.push(item);
-          }
-        });
+class CollectionView extends Component{
+  groupItems (items, itemsPerRow) {
+    var itemsGroups = [];
+    var group = [];
+    items.forEach(function (item) {
+      if (group.length === itemsPerRow) {
+        itemsGroups.push(group);
+        group = [item];
+      } else {
+        group.push(item);
+      }
+    });
 
-        if (group.length > 0) {
-          itemsGroups.push(group);
-        }
+    if (group.length > 0) {
+      itemsGroups.push(group);
+    }
 
-        return itemsGroups;
-    },
-    renderGroup: function(group) {
-      var that = this;
-      var items = group.map(function(item, index) {
-        return that.props.renderItem(item, index);
-      });
+    return itemsGroups;
+  }
+  renderGroup ({ item }) {
+    var that = this;
+    var items = item.map(function (obj, index) {
+      return that.props.renderItem(obj, index);
+    });
+    items = items.map((obj, i) => {
       return (
-        <View style={styles.group}>
-          {items}
-        </View>
-      );
-    },
-    render: function() {
-        var groups = this.groupItems(this.props.items, this.props.itemsPerRow);
-        var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        return (<ListView
-          {...this.props}
-          renderRow={this.renderGroup}
-          dataSource={ds.cloneWithRows(groups)}
-        />);
-    },
-});
+        <View style={{ flex: 1 }}>
+      {obj}
+    </View>)
+
+    })
+    return (
+      <View style={styles.group}>
+    {
+      items
+    }
+    {this.getBlankCell(item.length, this.props.itemsPerRow)
+    }
+
+  </View>
+  );
+  }
+
+  getBlankCell(currentItems, itemsPerRow) {
+
+    if (currentItems < itemsPerRow) {
+      var arr = []
+      for (var i = 0; i < itemsPerRow - currentItems; i++) {
+        arr.push(
+        <View style={{ flex: 1 }}>
+      </View>
+      )
+      }
+      return arr
+    }
+
+  }
+  render () {
+    var groups = this.groupItems(this.props.items, this.props.itemsPerRow);
+    return (<FlatList
+    data={groups}
+    {...this.props}
+    renderItem={this.renderGroup}
+    />);
+  }
+};
 
 
 var styles = StyleSheet.create({
   group: {
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center',
     overflow: 'hidden'
   }
 });
 
-module.exports = CollectionView;
+export default CollectionView;
